23e14b6c8858e9515dd1bdbb6e01fb55ade03cba
risano <muhammadrisano@gmail.com>
2021-05-22 09:22:23 +0700

    edit router

--- src/configs/Router/index.js
+++ src/configs/Router/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {BrowserRouter, Route} from 'react-router-dom'
 import Navbar from '../../components/Navbar'
 import Home from '../../pages/Home'
 import Add from '../../pages/Add'
