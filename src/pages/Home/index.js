18ef01cd2c86d2a879410aec8d9f17db7c39267a
risano <muhammadrisano@gmail.com>
2021-05-22 09:33:27 +0700

    edit home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,11 +2,7 @@ import React from 'react'
 import './home.css'
 import Card from '../../components/Card'
 function Home() {
-  const handleDelete = ()=>{
-    const confirmDelete = window.confirm('hello')
-    alert(confirmDelete)
 
-  }
   return (
     <div className="container">
       <Card
@@ -14,7 +10,6 @@ function Home() {
         price="10000"
         size="M"
         description="hello world"
-        onDelete={handleDelete}
       />
     </div>
   )
